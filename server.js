9c5e581568e927ba2625bbca70b1847f255bd9e6
Alessandro Alterno <alternonolimit@gmail.com>
2021-05-29 03:06:42 +0200

    Added dotenv

--- server.js
+++ server.js
@@ -8,11 +8,12 @@ const { graphqlHTTP } = require('express-graphql');
 const graphQlSchema = require('./schema/schema');
 const graphQlResolvers = require('./schema/resolvers/index');
 const app = express();
+require('dotenv').config();
 
 const mongoose = require('mongoose');
 const player = require('./schema/models/player');
 
-// const uri =  removed for security;
+const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASS + "@" +process.env.DB_HOST + "/" + process.env.DB_NAME + "?retryWrites=true&w=majority";
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection;
